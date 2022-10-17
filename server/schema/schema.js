f09be0856e7ea02349cbbf72b2bd2312e6accea0
Wamaitha <wamaithanyamu@gmail.com>
2022-10-17 19:01:59 +0300

    Feature: added relationship in the schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,6 @@
 import {projects, clients} from "../sampleData.js";
 
-import {GraphQLID, GraphQLObjectType, GraphQLSchema, GraphQLString} from "graphql";
+import {GraphQLID, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString} from "graphql";
 
 
 const ClientType = new GraphQLObjectType({
@@ -13,10 +13,52 @@ const ClientType = new GraphQLObjectType({
     })
 });
 
+const ProjectType = new GraphQLObjectType({
+    name:'Project',
+    fields: () => ({
+        id: {type: GraphQLID},
+        name: {type: GraphQLString},
+        description: {type: GraphQLString},
+        status: {type: GraphQLString},
+        client:{
+            type:ClientType,
+            resolve(parent, args){
+                return clients.find((client) => client.id === parent.clientId);
+
+            }
+
+        }
+    })
+});
+
 
 const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
+
     fields: {
+        projects:{
+            type: new GraphQLList(ProjectType),
+            resolve(parent, args){
+                return projects
+            }
+        },
+        project : {
+            type : ProjectType,
+            args : {
+                id: {type : GraphQLID}
+            },
+
+            resolve(parent,args){
+                return projects.find(project => project.id === args.id);
+            }
+        },
+
+        clients:{
+            type: new GraphQLList(ClientType),
+            resolve(parent, args){
+                return clients
+            }
+        },
         client : {
             type : ClientType,
             args : {
@@ -29,7 +71,7 @@ const RootQuery = new GraphQLObjectType({
         }
 
     }
-})
+});
 
 export const schema = new GraphQLSchema(({
     query : RootQuery
