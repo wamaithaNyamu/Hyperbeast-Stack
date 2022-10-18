7ab236b2aedaf86544b1424ebd01de89f424ef9f
Wamaitha <wamaithanyamu@gmail.com>
2022-10-18 11:26:49 +0300

    Feature: added the apollo provider for our root component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri:'http://localhost:5000/graphql',
+    cache: new InMemoryCache()
+
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+          <ApolloProvider client={client}>
+            <Header/>
+            <div className="container">
+              <h1>Hey baby</h1>
+            </div>
+          </ApolloProvider>
+    </>
   );
 }
 
